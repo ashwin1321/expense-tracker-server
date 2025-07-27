ee5b5670f1ed3ab747004536b5a21d9727d24ffe
ashwin1321 <ashwinkhatiwada7@gmail.com>
2025-07-27 16:31:58 +0545

    swagger

--- utils/swaggerdocs.js
+++ utils/swaggerdocs.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:8080/", 
+        url: "https://expense-tracker-server-mr2f.onrender.com/", 
       },
     ],
   },
